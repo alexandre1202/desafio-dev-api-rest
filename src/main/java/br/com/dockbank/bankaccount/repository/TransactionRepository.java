b1804bd2353554eb92c7ae78ffcf83c2c553eabf
alexandre1202 <alexandre1202@gmail.com>
2021-01-25 10:50:19 -0300

    Repository changes due to fix

--- src/main/java/br/com/dockbank/bankaccount/repository/TransactionRepository.java
+++ src/main/java/br/com/dockbank/bankaccount/repository/TransactionRepository.java
@@ -6,6 +6,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface TransactionRepository extends JpaSpecificationExecutor<JpaRepository>, JpaRepository<TransactionBank, Long> {
+public interface TransactionRepository extends JpaSpecificationExecutor<TransactionBank>, JpaRepository<TransactionBank, Long> {
     List<TransactionBank> findTransactionBanksByAccountBank_AccountIdAndAccountBank_AccountActiveAndTransactionCreatedEquals(Long accountId, String status, LocalDate localDate);
 }
